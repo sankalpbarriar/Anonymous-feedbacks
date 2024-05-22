dcf87b0a61a0d8d424649e25239250b30e948159
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2024-05-22 08:18:16 +0530

    user model created

---
+++ src/model/User.model.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+//typescript part
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+//message schema
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "verify code expiry is reqiured"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+
+  messages: [MessageSchema],
+});
+
+//exporting model
+//agar DB me already model hai to existing mil rha hai nahi to naya bana do
+const UserModel=(mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema);
+
+export default UserModel;
+
