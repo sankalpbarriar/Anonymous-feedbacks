ebe88febc8c0ec5d88024b9ace48beab52671ca7
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2024-06-12 22:16:01 +0530

    verify-user and uniqueName configured

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -3,6 +3,10 @@ import UserModel from "@/model/User.model";
 import { z } from "zod";
 import { usernameValidation } from "@/schemas/signUpSchema";
 
+//1. query ka schema
+//2. query nikalna hoga
+//3. safeParse(queryParams)
+
 //making querySchema
 const UsernameQuerySchema = z.object({
   username: usernameValidation,
@@ -13,12 +17,55 @@ const UsernameQuerySchema = z.object({
 export async function GET(request: Request) {
   await dbConnect();
 
+  //localhost:3000/api/cuu?username=sankalp
   try {
-    //checking username thorugh query url
-    const {searchParams}=new URL(request.url)
-    const queryParams={
-        username:searchParams.get('username')
+    //checking username through query url in NextJs
+    const { searchParams } = new URL(request.url); //url aagaya
+    const queryParams = {
+      //ab searchParams me se apna query nikalna hai
+      username: searchParams.get("username"),
+    };
+    //validate with zod
+    const result = UsernameQuerySchema.safeParse(queryParams);
+    console.log(result);
+    //agar result sahi nahi aaya
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
+        },
+        { status: 400 }
+      );
     }
+
+    const { username } = result.data;
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is available",
+      },
+      { status: 200 }
+    );
+
   } catch (error) {
     console.error("Error checking username ", error);
     return Response.json(
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,63 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 }
+      );
+    }
+    const isCodeValid=user.verifyCode===code;
+    const isCodeNotExpired=new Date(user.verifyCodeExpiry)>new Date()
+    if(isCodeValid && isCodeNotExpired){
+        user.isVerified=true;
+        await user.save();
+
+        return Response.json(
+            {
+              success: true,
+              message: "User verified successfully",
+            },
+            { status:200 }
+          );
+    }
+    else if(!isCodeNotExpired){
+        return Response.json(
+            {
+              success: false,
+              message: "Verification code has expired please sign up again to get a new one",
+            },
+            { status: 400 }
+          );
+    }
+    else{
+        return Response.json(
+            {
+              success: false,
+              message: "Incorrect verification code",
+            },
+            { status: 400 }
+          );
+    }
+
+
+  } catch (error) {
+    console.error("Error Verifying user ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error Verifying user ",
+      },
+      { status: 500 }
+    );
+  }
+}
